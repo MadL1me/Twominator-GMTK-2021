9d55e53f54ea532bb845591ec5c6676acde907ce
Ilushkins <46647517+Sn0wy-0wl@users.noreply.github.com>
2021-06-13 20:13:06 +0300

    abc

---
+++ GMTK-2021/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Rotator : MonoBehaviour
+    {
+        public void Update()
+        {
+            transform.Rotate(new Vector3(0,0,5));
+        }
+    }
+}
